54af08eaef59810b52dda3b7f27c9c789a0eb9ad
Tooni <sayers.neil@gmail.com>
2021-05-24 14:29:41 +0100

    Update pragmas in test files

--- codegen/cli.py
+++ codegen/cli.py
@@ -67,7 +67,7 @@ def main(args: typing.List[str]) -> int:
     if len(missing_pragmas) != 0:
         logger.ERROR(f"{scribble_filename} was missing the following pragmas: {missing_pragmas}")
         logger.INFO("Try adding them at the top of the file with: " +
-                    "(*# CheckDirectedChoiceDisabled, RefinementTypes, " +
+                    "(*# CheckDirectedChoiceDisabled, RefinementTypes, ValidateRefinementProgress, " +
                     "ValidateRefinementSatisfiability, ValidateRefinementSatisfiability #*)")
         return 1
 
